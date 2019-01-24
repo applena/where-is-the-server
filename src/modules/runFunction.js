4f17578bb634a178ba042e72b9bc00f5a04840d3
Fletcher LaRue <laruefletcher@gmail.com>
2019-01-24 13:54:33 -0800

    adds more error catching

--- src/modules/runFunction.js
+++ src/modules/runFunction.js
@@ -8,17 +8,18 @@ module.exports  = (request, response, next) => {
   let context = {body:request.body, env:process.env, param:request.params, query:request.query};
   let userFunction = require(`./../users/${username}/${functionName}`);
 
-  let output = userFunction(context);
+
+  try {
+    let output = userFunction(context);
   
-  if (parseJson(output)){ 
-    response.status(200).json(output);
-  }
-  else {
-    try {
-      response.status(200).send(output);
-    } catch (e) {
-      response.status(200).send({output});
+    if (parseJson(output)){ 
+      response.status(200).json(output);
     }
+
+    response.status(200).send(output);
+
+  } catch (e){
+    response.status(500).send('error running the function');
   }
  
 };
